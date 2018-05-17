41e19394d8be82fc54b1c4ae0926f861a1de479a
andyzlliu(刘志龙) <andyzlliu@tencent.com>
2018-05-17 15:14:56 +0800

    feat: init

---
+++ bubble.js
@@ -0,0 +1,17 @@
+/******************************************
+ *  Author : Author   
+ *  Created On : Sun May 13 2018
+ *  File : bubble.js
+ *******************************************/
+
+class Circle {  
+    constructor(options) {
+        this.x = 0;        
+        this.y = 0;
+    }    
+
+    move() {
+        this.x += 10; 
+        this.y += 10; 
+    } 
+}
